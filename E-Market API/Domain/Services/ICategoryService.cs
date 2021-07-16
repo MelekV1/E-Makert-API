0c4b5d5ef98c0be0e620ae5eaa11d953d97a4cf6
Malek Ammar <Malek.ammar@etudiant-enit.utm.tn>
2021-07-16 16:08:12 +0300

    first commit

---
+++ E-Market API/Domain/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using E_Market_API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Market_API.Domain.Services
+{
+    interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
